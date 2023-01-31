15d7d893b7fe7776234f03086ad894b04cdfb6c5
CindySong6 <cindysong622@gmail.com>
2023-01-30 22:00:19 -0500

    adding Part 2 Simple Class

--- assignment1_part2.py
+++ assignment1_part2.py
@@ -1,11 +1,15 @@
 
 class Book:
-    def __init__(self, author, title):
-        pass
-    
+    def __init__(self, author = "", title = ""):
+        self.author = author
+        self.title = title
+
     def display(self):
-        pass
+        print(self.title + ", written by " + self.author)
 
 
 if __name__ == "__main__":
-    pass
+    Book_One = Book("J. K. Rowling", "Harry Potter and the Goblet of Fire")
+    Book_Two = Book("Walter Scott", "Ivanhoe: A Romance")
+    Book_One.display()
+    Book_Two.display()
